0259a083221251dd3f290fce94cd3ec063a91a35
Kwonkyu <root@localhost>
2019-11-24 21:05:16 +0900

    #1. Rename periods     - 이전/오늘/내일/이번주 => 이전/오늘/이번주/일주일 이상     - added some strings
    #2. Modified layouts
        - Changed LinearLayout to ScrollView to make checklist activity scrollable
        - Modified element's id from previous/today/tomorrow/week => previous/today/week/far
        - Added some elements to checklist-item-add activity
    
    #3. FragmentChecklist
        - Removed unused variables, test codes and added some final int/String and enums.
        - Implemented item add function in onActivitResult
        - Implemented date comparator as dateComparator(). It takes date string as "yyyy/mm/dd".
        - Renamed variables as periods are renamed in #1.
        - Disabled nest scrolling in recyclerview to make scrolling in ScrollView smooth.

--- teamproject/app/src/main/java/com/example/teamproject/FragmentChecklist.java
+++ teamproject/app/src/main/java/com/example/teamproject/FragmentChecklist.java
@@ -1,5 +1,6 @@
 package com.example.teamproject;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,14 +17,25 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class FragmentChecklist extends Fragment {
-    public static final int CHECKLIST_ADD_ITEM_REQUEST = 1000;
-    private RecyclerView checklist_previous, checklist_today, checklist_tomorrow, checklist_week;
-    private RecyclerView.Adapter previous_adapter, today_adapter, tomorrow_adapter, week_adapter;
-    private RecyclerView.LayoutManager previous_manager, today_manager, tomorrow_manager, week_manager;
-    private ArrayList<String> previous, today, tomorrow, week;
+    static final int CHECKLIST_ADD_ITEM_REQUEST = 1000;
+    static final int CHECKLIST_ADD_ITEM_RESULT = 1001;
+    static final String CHECKLIST_ADD_ITEM_OK = "[OK]";
+    static final String CHECKLIST_ADD_ITEM_DATE = "[DATE]";
+    private enum RangeCategory {BEFORE, TODAY, WEEK, FAR}
+
+    private RecyclerView checklist_previous, checklist_today, checklist_week, checklist_far;
+    private RecyclerView.Adapter previous_adapter, today_adapter, week_adapter, far_adapter;
+    private RecyclerView.LayoutManager previous_manager, today_manager, week_manager, far_manager;
+    private ArrayList<String> previous, today, week, far;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -50,63 +63,117 @@ public class FragmentChecklist extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode){
+            case CHECKLIST_ADD_ITEM_REQUEST:
+                switch(resultCode){
+                    case CHECKLIST_ADD_ITEM_RESULT:
+                        String itemText = data.getStringExtra(CHECKLIST_ADD_ITEM_OK);
+                        String expireDate = data.getStringExtra(CHECKLIST_ADD_ITEM_DATE);
+                        Calendar cal = Calendar.getInstance();
+                        String currentDate = String.format(Locale.KOREAN,"%d/%d/%d",
+                                cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
+
+                        switch(dateComparator(currentDate, expireDate)){
+                            case BEFORE:
+                                previous.add(itemText);
+                                previous_adapter.notifyDataSetChanged();
+                                break;
+
+                            case TODAY:
+                                today.add(itemText);
+                                today_adapter.notifyDataSetChanged();
+                                break;
+
+                            case WEEK:
+                                week.add(itemText);
+                                week_adapter.notifyDataSetChanged();
+                                break;
+
+                            case FAR:
+                                far.add(itemText);
+                                far_adapter.notifyDataSetChanged();
+                                break;
+
+                            default:
+                                Toast.makeText(getContext(), "Error on adding item", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+
+                    default:
+                        Toast.makeText(getContext(), "Item add failed", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_checklist, container, false);
         previous = new ArrayList<>();
         today = new ArrayList<>();
-        tomorrow = new ArrayList<>();
         week = new ArrayList<>();
-//      ----------test-------------
-        previous.add("Do laundry");
-        today.add("Go to school");
-        today.add("Buy some foods");
-        today.add("Buy more foods");
-        today.add("Buy more foods");
-        today.add("Buy more foods");
-        today.add("Buy more foods");
-        today.add("Buy more foods");
-        today.add("Buy more foods");
-        // tomorrow.add("Do nothing on tomorrow");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-        week.add("Interview with Manager");
-//        -------------------------
+        far = new ArrayList<>();
+
+//        test();
 
         checklist_previous = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_previous);
-        checklist_previous.setHasFixedSize(true);
         checklist_today = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_today);
-        checklist_today.setHasFixedSize(true);
-        checklist_tomorrow = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_tomorrow);
-        checklist_tomorrow.setHasFixedSize(true);
         checklist_week = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_week);
-        checklist_week.setHasFixedSize(true);
+        checklist_far = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_far);
+
+        checklist_previous.setNestedScrollingEnabled(false);
+        checklist_today.setNestedScrollingEnabled(false);
+        checklist_week.setNestedScrollingEnabled(false);
+        checklist_far.setNestedScrollingEnabled(false);
 
         previous_manager = new LinearLayoutManager(getActivity());
         today_manager = new LinearLayoutManager(getActivity());
-        tomorrow_manager = new LinearLayoutManager(getActivity());
         week_manager = new LinearLayoutManager(getActivity());
+        far_manager = new LinearLayoutManager(getActivity());
 
         checklist_previous.setLayoutManager(previous_manager);
         checklist_today.setLayoutManager(today_manager);
-        checklist_tomorrow.setLayoutManager(tomorrow_manager);
         checklist_week.setLayoutManager(week_manager);
+        checklist_far.setLayoutManager(far_manager);
 
         previous_adapter = new CheckListAdapter(previous);
         today_adapter = new CheckListAdapter(today);
-        tomorrow_adapter = new CheckListAdapter(tomorrow);
         week_adapter = new CheckListAdapter(week);
+        far_adapter = new CheckListAdapter(far);
 
         checklist_previous.setAdapter(previous_adapter);
         checklist_today.setAdapter(today_adapter);
-        checklist_tomorrow.setAdapter(tomorrow_adapter);
         checklist_week.setAdapter(week_adapter);
+        checklist_far.setAdapter(far_adapter);
 
         return rootView;
+    }
+
+    @TargetApi(26)
+    private RangeCategory dateComparator(String current, String target){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d");
+        // DD means day of year. MM is not proper. Use symbol properly.
+        // Check at https://developer.android.com/reference/java/time/format/DateTimeFormatter
+        LocalDate currentDate = LocalDate.parse(current, formatter);
+        LocalDate targetDate = LocalDate.parse(target, formatter);
+
+        if(currentDate.isAfter(targetDate)){
+            return RangeCategory.BEFORE;
+        } else if(currentDate.isEqual(targetDate)){
+            return RangeCategory.TODAY;
+        } else if(targetDate.isAfter(currentDate)){
+            long between = DAYS.between(currentDate, targetDate);
+            if(between <= 7){
+                return RangeCategory.WEEK;
+            } else {
+                return RangeCategory.FAR;
+            }
+        }
+
+        return RangeCategory.TODAY;
     }
 }
