ad6cac427dc161f47c0779588655c8144702ef12
Kwonkyu <root@localhost>
2019-11-25 21:27:59 +0900

    #1. Implemented checklist item's class and layout     - Getter and Setter for checklist item's text and date.     - Constructor for LocalDate and String type of expiration date.     - TextView checklist_item_date below of checklist_item     - Applied changes in FragmentChecklist.java
    #2. Marked and unmarked state for checklist item
        - Implemented in ViewHolder's constructor with setOnClickListener(), setBackground()
    
    #3. etc
        - Changed color and padding of RecyclerView's category.
        - Added annotation(comments?) in source codes.
    
    #4. TODO
        - Implement checklist item backup and load feature
        - It should be loaded and saved on onResume() or onStop()

--- teamproject/app/src/main/java/com/example/teamproject/CheckListItemAdd.java
+++ teamproject/app/src/main/java/com/example/teamproject/CheckListItemAdd.java
@@ -12,10 +12,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -29,34 +26,27 @@ public class CheckListItemAdd extends AppCompatActivity {
     private TextView dateInput;
 
     public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-        boolean init = false;
         TextView dateText;
         void setTextView(TextView date) {
             dateText = date;
         }
-
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            if(!init) {
-
-                init = true;
-            }
-            super.onCreate(savedInstanceState);
-        }
+        // link date string from constructor.
 
         @Override @NonNull @TargetApi(26)
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // TODO: How to init DatePicker's default as today, and keep user's selection after dialog is closed?
-            // TODO: Need a clear, simple and effective way to translate dates between textview and intent's data.
             String[] ymd = dateText.getText().toString().split("/");
+            // Split yyyy/mm/dd format date string with "/" delimiter.
             int[] ymdInt = new int[3];
             for(int i=0;i<3;i++) ymdInt[i] = Integer.parseInt(ymd[i]);
+            // Parse date string element(year, month, day) to Integer.
             return new DatePickerDialog(getActivity(), this, ymdInt[0], ymdInt[1]-1, ymdInt[2]);
+            // month should be decreased by 1 because of 0-indexing. See onDateSet().
         }
 
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
             dateText.setText(String.format(Locale.KOREAN, "%d/%d/%d", year, monthOfYear+1, dayOfMonth));
+            // Set date string with increased month(by 1).
         }
     }
 
@@ -66,6 +56,7 @@ public class CheckListItemAdd extends AppCompatActivity {
 
         input = findViewById(R.id.checklist_edit_add);
         dateInput = findViewById(R.id.checklist_text_date);
+        // Link view to variable
 
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -73,6 +64,7 @@ public class CheckListItemAdd extends AppCompatActivity {
         int day = cal.get(Calendar.DAY_OF_MONTH);
         String currentDate = String.format(Locale.KOREAN, "%d/%d/%d", year, month + 1, day);
         dateInput.setText(currentDate);
+        // Get today's date as format of yyyy/mm/dd string and set.
 
         final DatePickerFragment dpf = new DatePickerFragment();
         dpf.setTextView(dateInput);
@@ -83,6 +75,7 @@ public class CheckListItemAdd extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dpf.show(getSupportFragmentManager(), "datePicker");
+                // Show DatePickerDialogFragment
             }
         });
         super.onCreate(savedInstanceState);
@@ -103,6 +96,7 @@ public class CheckListItemAdd extends AppCompatActivity {
                 intent.putExtra(FragmentChecklist.CHECKLIST_ADD_ITEM_OK, text);
                 intent.putExtra(FragmentChecklist.CHECKLIST_ADD_ITEM_DATE, dateInput.getText().toString());
                 setResult(FragmentChecklist.CHECKLIST_ADD_ITEM_RESULT, intent);
+                // Get checklist item's text and expiration date as string and send as intent's data.
                 finish();
         }
         return super.onOptionsItemSelected(item);
--- teamproject/app/src/main/java/com/example/teamproject/FragmentChecklist.java
+++ teamproject/app/src/main/java/com/example/teamproject/FragmentChecklist.java
@@ -17,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.FileOutputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -30,12 +31,14 @@ public class FragmentChecklist extends Fragment {
     static final int CHECKLIST_ADD_ITEM_RESULT = 1001;
     static final String CHECKLIST_ADD_ITEM_OK = "[OK]";
     static final String CHECKLIST_ADD_ITEM_DATE = "[DATE]";
+    static final String SAVE_FILENAME = "checklist_save.txt";
     private enum RangeCategory {BEFORE, TODAY, WEEK, FAR}
-
+    // Constant int and string used in Intent, File I/O, etc...
     private RecyclerView checklist_previous, checklist_today, checklist_week, checklist_far;
     private RecyclerView.Adapter previous_adapter, today_adapter, week_adapter, far_adapter;
     private RecyclerView.LayoutManager previous_manager, today_manager, week_manager, far_manager;
-    private ArrayList<String> previous, today, week, far;
+    private ArrayList<CheckListItem> previous, today, week, far;
+    // Recycler related variables and data structure for storing checklist items.
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -44,15 +47,29 @@ public class FragmentChecklist extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        // TODO: implement checklist backup file load feature
+        // TODO: checklist item should be ordered and categorized by expiration date
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        // TODO: implement checklist backup file save feature
+//        FileOutputStream fos = getContext().openFileOutput(SAVE_FILENAME, Context.MODE_PRIVATE);
+        super.onStop();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
         inflater.inflate(R.menu.checklist_menu, menu);
-//        super.onCreateOptionsMenu(menu, inflater);
+        // apply checklist menu
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
-            case R.id.checklist_menu_add:
+            case R.id.checklist_menu_add: // call checklist item add activity
                 Intent intent = new Intent(getActivity(), CheckListItemAdd.class);
                 startActivityForResult(intent, CHECKLIST_ADD_ITEM_REQUEST);
                 break;
@@ -66,33 +83,36 @@ public class FragmentChecklist extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch(requestCode){
-            case CHECKLIST_ADD_ITEM_REQUEST:
+            case CHECKLIST_ADD_ITEM_REQUEST: // result of called activity
                 switch(resultCode){
-                    case CHECKLIST_ADD_ITEM_RESULT:
+                    case CHECKLIST_ADD_ITEM_RESULT: // properly added from called activity
                         String itemText = data.getStringExtra(CHECKLIST_ADD_ITEM_OK);
                         String expireDate = data.getStringExtra(CHECKLIST_ADD_ITEM_DATE);
+                        // Get checklist item text and expiration date
                         Calendar cal = Calendar.getInstance();
                         String currentDate = String.format(Locale.KOREAN,"%d/%d/%d",
                                 cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1, cal.get(Calendar.DAY_OF_MONTH));
-
+                        // Parse today's date to string in format of yyyy/mm/dd using
                         switch(dateComparator(currentDate, expireDate)){
+                            // Compare dates(today, expiration date), add in corresponding RecyclerView.
+                            // Checklist item is instantiated as CheckListItem class.
                             case BEFORE:
-                                previous.add(itemText);
+                                previous.add(new CheckListItem(itemText, expireDate));
                                 previous_adapter.notifyDataSetChanged();
                                 break;
 
                             case TODAY:
-                                today.add(itemText);
+                                today.add(new CheckListItem(itemText, expireDate));
                                 today_adapter.notifyDataSetChanged();
                                 break;
 
                             case WEEK:
-                                week.add(itemText);
+                                week.add(new CheckListItem(itemText, expireDate));
                                 week_adapter.notifyDataSetChanged();
                                 break;
 
                             case FAR:
-                                far.add(itemText);
+                                far.add(new CheckListItem(itemText, expireDate));
                                 far_adapter.notifyDataSetChanged();
                                 break;
 
@@ -100,9 +120,6 @@ public class FragmentChecklist extends Fragment {
                                 Toast.makeText(getContext(), "Error on adding item", Toast.LENGTH_SHORT).show();
                         }
                         break;
-
-                    default:
-                        Toast.makeText(getContext(), "Item add failed", Toast.LENGTH_SHORT).show();
                 }
                 break;
 
@@ -117,38 +134,44 @@ public class FragmentChecklist extends Fragment {
         today = new ArrayList<>();
         week = new ArrayList<>();
         far = new ArrayList<>();
+        // data structures for RecyclerView.
 
-//        test();
-
+        // TODO: Sort items by dates.
         checklist_previous = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_previous);
         checklist_today = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_today);
         checklist_week = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_week);
         checklist_far = (RecyclerView)rootView.findViewById(R.id.checklist_recycler_far);
+        // link views to variables.
 
         checklist_previous.setNestedScrollingEnabled(false);
         checklist_today.setNestedScrollingEnabled(false);
         checklist_week.setNestedScrollingEnabled(false);
         checklist_far.setNestedScrollingEnabled(false);
+        // disable nested scroll for smooth scrolling of ScrollView.
 
         previous_manager = new LinearLayoutManager(getActivity());
         today_manager = new LinearLayoutManager(getActivity());
         week_manager = new LinearLayoutManager(getActivity());
         far_manager = new LinearLayoutManager(getActivity());
+        // construct layout managers for each view.
 
         checklist_previous.setLayoutManager(previous_manager);
         checklist_today.setLayoutManager(today_manager);
         checklist_week.setLayoutManager(week_manager);
         checklist_far.setLayoutManager(far_manager);
+        // apply LinearLayoutManager to each view.
 
         previous_adapter = new CheckListAdapter(previous);
         today_adapter = new CheckListAdapter(today);
         week_adapter = new CheckListAdapter(week);
         far_adapter = new CheckListAdapter(far);
+        // construct custom Adapter
 
         checklist_previous.setAdapter(previous_adapter);
         checklist_today.setAdapter(today_adapter);
         checklist_week.setAdapter(week_adapter);
         checklist_far.setAdapter(far_adapter);
+        // apply Adapter to each view.
 
         return rootView;
     }
@@ -160,13 +183,16 @@ public class FragmentChecklist extends Fragment {
         // Check at https://developer.android.com/reference/java/time/format/DateTimeFormatter
         LocalDate currentDate = LocalDate.parse(current, formatter);
         LocalDate targetDate = LocalDate.parse(target, formatter);
+        // Parse LocalDate instance from given date string.
 
+        // Compare dates
         if(currentDate.isAfter(targetDate)){
             return RangeCategory.BEFORE;
         } else if(currentDate.isEqual(targetDate)){
             return RangeCategory.TODAY;
         } else if(targetDate.isAfter(currentDate)){
             long between = DAYS.between(currentDate, targetDate);
+            // If period between two dates are less than a week or not.
             if(between <= 7){
                 return RangeCategory.WEEK;
             } else {
@@ -176,4 +202,5 @@ public class FragmentChecklist extends Fragment {
 
         return RangeCategory.TODAY;
     }
+
 }
